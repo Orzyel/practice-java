8287f0547acba5cb8806cdf4a6c5215ac27e1782
Anthony <ant.cammy@gmail.com>
2020-02-22 14:50:46 -0500

    gitignore

--- src/com/company/InvoiceTest.java
+++ src/com/company/InvoiceTest.java
@@ -4,5 +4,7 @@ public class InvoiceTest {
     public static void main(String[] args) {
         Invoice invoice = new Invoice("dv343", "Watch", 3, 4200);
         System.out.println("The quantity of the invoice is " + invoice.getQuantity());
+
+        //hallo
     }
 }
